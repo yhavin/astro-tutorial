d8fb592147c008192c33044774506f320ca0588f
yhavin <y.havin@gmail.com>
2024-09-26 16:42:34 -0400

    Add hydrated Greeting component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astro-tutorial-yhavin.netlify.app"
+  site: "https://astro-tutorial-yhavin.netlify.app",
+  integrations: [preact()]
 });
